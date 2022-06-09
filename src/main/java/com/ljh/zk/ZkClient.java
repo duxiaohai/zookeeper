4e69851cadda766f8061169e8ca515a1fefd1afe
linjh <linjh@primeton.com>
2022-06-10 00:32:37 +0800

    zookeeper

--- src/main/java/com/ljh/zk/ZkClient.java
+++ src/main/java/com/ljh/zk/ZkClient.java
@@ -1,6 +1,7 @@
 package com.ljh.zk;
 
 import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -22,7 +23,19 @@ public class ZkClient {
     public void init() throws IOException {
             zkClient = new ZooKeeper(connectString, sessionTimeOut, new Watcher() {
             public void process(WatchedEvent watchedEvent) {
-
+//                System.out.println("--------------------------------------");
+//                List<String> childrenList = null;
+//                try {
+//                    childrenList = zkClient.getChildren("/", true);
+//                    childrenList.forEach(children ->{
+//                        System.out.println(children);
+//                    });
+//                    System.out.println("----------------------------------");
+//                } catch (KeeperException e) {
+//                    e.printStackTrace();
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
             }
         });
     }
@@ -36,10 +49,17 @@ public class ZkClient {
     @Test
     public void getChildren() throws KeeperException, InterruptedException {
         List<String> childrenList = zkClient.getChildren("/", true);
-        childrenList.forEach(children ->{
-            System.out.println(children);
-        });
-
+//        childrenList.forEach(children ->{
+//            System.out.println(children);
+//        });
+        Thread.sleep(Long.MAX_VALUE);
     }
+
+    @Test
+    public void exit() throws KeeperException, InterruptedException {
+        Stat exists = zkClient.exists("/atguigu", false);
+        System.out.println(exists ==null ? "not exit": "exit");
+    }
+
 
 }
